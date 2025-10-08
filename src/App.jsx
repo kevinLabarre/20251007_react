07d38d6758e64c6b0af180615ea8c317d09fae5d
klabarre <klabarre@dawan.fr>
2025-10-08 11:54:38 +0200

    Mise en place des routes (réorganisation du projet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { JsxTheorie } from "./components/JsxTheorie";
+import MyFirstComponent from "./components/MyFirstComponent";
+import HookUseState from "./components/HookUseState";
+import List from "./components/List";
+import Form from "./components/Form";
+import { ExerciceFruits } from "./components/Exercice/ExerciceFruits";
 
 function App() {
-  return <JsxTheorie />;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<JsxTheorie />} />
+          <Route
+            path="/passage-donnee-parent-enfant"
+            element={<MyFirstComponent />}
+          />
+          <Route path={"/hook-use-state"} element={<HookUseState />} />
+          <Route path={"/parcourir-list-avac-map"} element={<List />} />
+          <Route path={"/formulaire"} element={<Form />} />
+          <Route
+            path={"Exercice-fruits-legumes"}
+            element={<ExerciceFruits />}
+          />
+
+          <Route path={"*"} element={<h1>Page 404 !</h1>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
-
-// Jsx théorie
-
-//  <MyFirstComponent />
-
-//       <HookUseState />
-
-//       <List />
-
-//       <Form />
-
-//       <ExerciceFruits />
