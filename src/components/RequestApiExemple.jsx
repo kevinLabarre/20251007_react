29f4442cd6dafb71672775fcb5543c34b77dcedb
klabarre <klabarre@dawan.fr>
2025-10-08 16:23:29 +0200

    exemple requete _ partie 2

--- src/components/RequestApiExemple.jsx
+++ src/components/RequestApiExemple.jsx
@@ -1,3 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const RequestApiExemple = () => {
-  return <>{/* Contenu de votre composant */}</>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/products") // Quand on ne souhaite pas gérer les erreurs, on peut utiliser que le .then
+      .then((resp) => {
+        setProducts(resp.data);
+        console.log(resp.data);
+      });
+  }, []);
+
+  axios
+    .get("http://localhost:3001/products")
+    .then((resp) => console.log(resp.data)) // fonction se déclenche uniquement si j'ai un retour sans erreur
+    .catch((err) => console.error(err)) // fonction se déclenche uniquement si j'ai une erreur
+    .finally(() => console.log("réponse reçue"));
+
+  return (
+    <>
+      <p>Tableau de produits: </p>
+      {products.length > 0 && <p>info du 1er produit: {products[0].name}</p>}
+
+      {/* { products.map((p) => ) } */}
+    </>
+  );
 };
