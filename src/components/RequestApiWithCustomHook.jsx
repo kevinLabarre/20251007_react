6646c98285bfa8711185a51dac0776c90631630e
klabarre <klabarre@dawan.fr>
2025-10-09 11:52:23 +0200

    utilisation du hook 'axios'  dans un composant

--- src/components/RequestApiWithCustomHook.jsx
+++ src/components/RequestApiWithCustomHook.jsx
@@ -1,4 +1,14 @@
+import { useProducts } from "../hooks/useProducts";
+
 export const RequestApiWithCustomHook = () => {
+  const { getProducts } = useProducts();
+
+  getProducts().then((resp) =>
+    console.log("En utilisant le hook: ", resp.data)
+  );
+
+  // Afficher sur la page les produits que l'on voit dans notre console
+
   return (
     <>
       <h1>Récupération avec un hook personnalisé</h1>
